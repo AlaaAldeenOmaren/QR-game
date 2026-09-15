7c79d29ad53fd17a58f5fdfe0647cdac597f7371
Alaa Aldeen Omaren <d302707@edu.curio.nl>
2026-09-15 10:09:10 +0200

    add home and login routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\OrganizerAuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrganizerDashboardController;
+use App\Http\Controllers\GameStateController;
+use App\Http\Controllers\OrganizerQuestionController;
+use App\Http\Controllers\StudentQuestionController;
+use App\Http\Controllers\OrganizerGradingController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home')->name('home');
+
+Route::middleware('guest')->group(function (): void {
+    Route::get('/beheer/inloggen', [
+        OrganizerAuthController::class,
+        'create',
+    ])->name('login');
+
+    Route::post('/beheer/inloggen', [
+        OrganizerAuthController::class,
+        'store',
+    ])->middleware('throttle:5,1')->name('login.store');
 });
+
+Route::middleware('auth')->group(function (): void {
+    Route::get('/beheer', [
+        OrganizerDashboardController::class,
+        'index',
+    ])->name('dashboard');
+
